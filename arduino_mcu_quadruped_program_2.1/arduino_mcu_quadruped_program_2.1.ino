05f8e979afc7e7624bd491ea8cbc242efca51f6c
slawomirkorbas <slawomirkorbas@gmail.com>
2021-03-20 18:27:34 +0100

    inverse kinematics approach

--- arduino_mcu_quadruped_program_2.1/arduino_mcu_quadruped_program_2.1.ino
+++ arduino_mcu_quadruped_program_2.1/arduino_mcu_quadruped_program_2.1.ino
@@ -34,11 +34,9 @@ class DynamixelServo
 {
   public:
     int id;
-    float homePos;
 
-    DynamixelServo(int servoId, float h) {
+    DynamixelServo(int servoId) {
       this->id = servoId;
-      this->homePos = h;
     }
 
     void printStatus() {
@@ -73,8 +71,14 @@ class DynamixelServo
         }
     }
 
+    void setHomingOffset(double offsetDeg) {
+      uint32_t offset = offsetDeg/0.088; // Unit 0.088
+      uint32_t writeTimeout = 100; // ms
+      shield.writeControlTableItem(HOMING_OFFSET, id, offset, writeTimeout);
+    }
 
-    void setPositionMode(int mode) {
+
+    void setOperatingMode(int mode) {
           shield.torqueOff(id);
           shield.setOperatingMode(id, mode);
           shield.torqueOn(id);
@@ -99,26 +103,17 @@ class DynamixelServo
            Serial.print("Servo ping successfull: ");
            Serial.println(id);
        }
-     }
-
-     void printPosition() {
-          DEBUG_SERIAL.print("Positon of servo ");
-          DEBUG_SERIAL.print(id);
-          DEBUG_SERIAL.print(" is ");
-          DEBUG_SERIAL.print(shield.getPresentPosition(id));
-          DEBUG_SERIAL.println("");
-     }
-             
+     }             
 
      void homePosition() {
-        shield.setGoalPosition(id, homePos, UNIT_DEGREE);
+        shield.setGoalPosition(id, 180, UNIT_DEGREE);
      }
 
-     void setPositionRelative(float offsetDeg) {
-       shield.setGoalPosition(id, homePos + offsetDeg, UNIT_DEGREE);
+     void setAngleRelative(float offsetDeg) {
+       shield.setGoalPosition(id, 180 + offsetDeg, UNIT_DEGREE);
      }
 
-     void setAngle(double angle) {
+     void setAngle(float angle) {
         shield.setGoalPosition(id, angle, UNIT_DEGREE);
         DEBUG_SERIAL.println(" ");
         DEBUG_SERIAL.print("angle ");
@@ -132,25 +127,26 @@ class DynamixelServo
 class Leg
 {
    private:
-      boolean inv1, inv2, inv3;
+      boolean inv;
       DynamixelServo *servos[3];
+      DynamixelServo *hip;
+      DynamixelServo *femur;
+      DynamixelServo *tibia;
 
    public:
-     Leg(int s1, int s2, int s3, float h1, float h2, float h3) {
-        servos[0] = new DynamixelServo(s1,h1);
-        servos[1] = new DynamixelServo(s2,h2);
-        servos[2] = new DynamixelServo(s3,h3);
-     }
-
-     void configureInversion(boolean i01, boolean i02, boolean i03) {
-        inv1 = i01;
-        inv2 = i02;
-        inv3 = i03;
+     Leg(int s1, int s2, int s3, boolean inverted) {
+        servos[0] = new DynamixelServo(s1);
+        hip = servos[0];
+        servos[1] = new DynamixelServo(s2);
+        femur = servos[1];
+        servos[2] = new DynamixelServo(s3);
+        tibia = servos[2];
+        this->inv = inverted;
      }
   
      void enablePositionMode() {
         for(int i=0; i<3; i++) {
-          servos[i]->setPositionMode(OP_POSITION);
+          servos[i]->setOperatingMode(OP_POSITION);
         }
      }
 
@@ -159,53 +155,57 @@ class Leg
              servos[i]->setSpeed(speedPct); 
           }
      }
-  
-     void setHome() {
-         for(int i=0; i<3; i++) {
-            servos[i]->homePosition();
-         }
-     }
+ 
+     void moveTo(float targetX, float targetY) {
+
+        float femurAngle, tibiaAngle;
+        float femurLen = 9.6;  //cm
+        float tibiaLen = 11.0; //cm
+        float dist = sqrt(sq(targetX) + sq(targetY));
+        tibiaAngle = angleFromCosineRuleDeg(femurLen, tibiaLen, dist);
+        float beta1 = angleFromCosineRuleDeg(dist, femurLen, tibiaLen);
+        float beta2 = toDeg(atan(targetX/targetY));
+        femurAngle = abs(beta2 - beta1);
+        
+          Serial.println("-----");
+          Serial.print("beta1 ");
+          Serial.println(beta1);
+          Serial.print("beta2 ");
+          Serial.println(beta2);
+
+        //hip->setAngle(hipAngle);
+        femur->setAngle(this->inv ? 180 - femurAngle : 180 + femurAngle);
+        tibia->setAngle(this->inv ? 360 - tibiaAngle : tibiaAngle);
+    }
+
+     /*
+     * Calculates angle using law of cosine for known triangle edges
+     */
+    float angleFromCosineRuleDeg(float a, float b, float c) {
+      float radAngle = safe_acos((sq(a) + sq(b) - sq(c))/(2*a*b));
+      return toDeg(radAngle);
+    }
+
+    float toDeg(float rad){
+      return (rad * (180/3.1415));
+    }
+    
+    
+    double safe_acos(double value) {
+        if (value<=-1.0) {
+            return 3.1415;
+        } else if (value>=1.0) {
+            return 0;
+        } else {
+            return acos(value);
+        }
+    }
 
      void ping() {
          for(int i=0; i<3; i++) {
             servos[i]->ping();
             servos[i]->printStatus();
          }
-     }
-
-     void printPosition() {
-         for(int i=0; i<3; i++) {
-            servos[i]->printPosition();
-         }
-     }
-
-     void setAngles(float timurAngle, float femurAngle) {
-        servos[1]->setAngle(femurAngle);
-        servos[2]->setAngle(timurAngle);
-     }
-
-     void calfUp(double deg) {
-        servos[2]->setPositionRelative(inv3 ? -deg : deg);
-     }
-
-     void calfDown(double deg) {
-        servos[2]->setPositionRelative( -1* (inv3 ? -deg : deg));
-     }
-
-     void tighBack(double deg) {
-        servos[1]->setPositionRelative(inv2 ? -deg : deg);
-     }
-
-     void tighForward(double deg) {
-        servos[1]->setPositionRelative(-1*(inv2 ? -deg : deg));
-     }
-
-     void hipInside(double deg ) {
-        servos[0]->setPositionRelative((inv1 ? -deg : deg));
-     }
-
-     void hipOutside(double deg) {
-       servos[0]->setPositionRelative(-1*(inv1 ? -deg : deg));
      }
      
 };
@@ -221,60 +221,17 @@ static const String CMD_TURN_RIGHT = "TURN_RIGHT";
 static const String CMD_GREET = "GREET";
 static const String CMD_FOOTWORK = "FOOTWORK";
 static const String CMD_SWING = "SWING";
-/**
- * Calculates angle using law of cosine for known triangle edges
- */
-static double angleFromCosineRuleDeg(double a, double b, double c) {
 
-
-   
-   Serial.println("cos value : ");
-   Serial.print("sq(a) ");
-   Serial.println(sq(a));
-      Serial.print("sq(b) ");
-   Serial.println(sq(b));
-      Serial.print("sq(c) ");
-   Serial.println(sq(c));
-      Serial.print("2*a*b ");
-   Serial.println(2*a*b);
-   Serial.println("       ");
-    Serial.print((sq(a) + sq(b) - sq(c))/(2*a*b) );
-    Serial.println("       ");
-  double radAngle = safe_acos( (sq(a) + sq(b) - sq(c))/(2*a*b) );
-  Serial.println("Rad angle: ");
-  Serial.println(radAngle);
-  return (radAngle * (180/3.1415)); //convert to degrees
-}
-
-static double calcDistance2D(double a, double b) {
-  return sqrt(sq(a) + sq(b));
-}
-
-static double safe_acos(double value) {
-    if (value<=-1.0) {
-        return 3.1415;
-    } else if (value>=1.0) {
-        return 0;
-    } else {
-        return acos(value);
-    }
-}
 
 /**
  * Main Quadruped robot class.
  */
 class QuadrupedRobot {
   public:
-    Leg leg_fR = Leg(10, 11, 12,  179.0,   120.0,   80.0 ); 
-    Leg leg_fL = Leg(20, 21, 22,  180.0,   240.0,   280.0 );
-    Leg leg_rL = Leg(41, 40, 55,  180.0,   340.0,   100.0 );   //285 ,180 -> angle "0"
-    Leg leg_rR = Leg(31, 30, 32,  180.0,   125.0,   260.0 );
-
-//          leg_fR.configureInversion(false, true,  false);
-//      leg_fL.configureInversion(false, false, true);
-//      leg_rL.configureInversion(false, false, true);
-//      leg_rR.configureInversion(false, true,  false);
-//    
+    Leg leg_fR = Leg(10, 11, 12, true);  
+    Leg leg_rR = Leg(31, 30, 32, true);
+    Leg leg_fL = Leg(20, 21, 22, false);
+    Leg leg_rL = Leg(41, 40, 55, false);  
 
 
     String currenlyExecutedCommand;
@@ -302,27 +259,23 @@ class QuadrupedRobot {
         return commandFromJetson;
     }
 
-  /**
-   * Servo smoke test and configuration...
-   */
+    /**
+    * Servo smoke test and configuration...
+    */
     void configure() 
     {
-      Serial.println("Robot configuration started...");
-      leg_fR.ping();
-      leg_fL.ping();
-      leg_rL.ping();
-      leg_rR.ping();
-  
-      leg_fR.enablePositionMode();
-      leg_fL.enablePositionMode();
-      leg_rL.enablePositionMode();
-      leg_rR.enablePositionMode();
- 
-      leg_fR.configureInversion(false, true,  false);
-      leg_fL.configureInversion(false, false, true);
-      leg_rL.configureInversion(false, false, true);
-      leg_rR.configureInversion(false, true,  false);
-      Serial.println("Robot configuration finished...");
+        Serial.println("Robot configuration started...");
+        leg_fR.ping();
+        leg_fL.ping();
+        leg_rL.ping();
+        leg_rR.ping();
+    
+        leg_fR.enablePositionMode();
+        leg_fL.enablePositionMode();
+        leg_rL.enablePositionMode();
+        leg_rR.enablePositionMode();
+   
+        Serial.println("Robot configuration finished...");
     }
 
     void setSpeed(double percentage)
@@ -347,77 +300,68 @@ class QuadrupedRobot {
 
     void homePosition()
     {
-          Serial.println("Setting Home position");
-          leg_fR.setHome();
-          leg_fL.setHome();
-          leg_rL.setHome();
-          leg_rR.setHome();
-          delay(500);
+       leg_rL.moveTo(1, 14);
+       leg_fL.moveTo(1, 14);
+       leg_rR.moveTo(1, 14);
+       leg_fR.moveTo(1, 14);
+       
     }
-
-    void moveLegTo(double targetX, double targetY) {
-
-        Serial.println("Moving leg to ...");
-        double femurLen = 97;  //mm
-        double tibiaLen = 110; //mm
-        double dist = calcDistance2D(targetX, targetY);
-         Serial.println("Distance ");
-        Serial.println(dist);
-        double tibiaAngle = angleFromCosineRuleDeg(femurLen, tibiaLen, dist);
-        Serial.println(tibiaAngle);
-
-        double betaTotal = angleFromCosineRuleDeg(dist, femurLen, tibiaLen);
-        Serial.println(betaTotal);
-        double beta2 = angleFromCosineRuleDeg(dist, targetX, targetY);
-        Serial.println(beta2);
-        double femurAngle = betaTotal - beta2;
-
-        //285 ,180
-        femurAngle = 285 + femurAngle;
-        tibiaAngle = 180 - tibiaAngle;
-        Serial.println("   ");
-        Serial.println("femurAngle: ");
-        Serial.print(femurAngle);
-        Serial.println("tibiaAngle: ");
-        Serial.print(tibiaAngle);
-        Serial.println("  ");
-        leg_rL.setAngles(femurAngle, tibiaAngle);
-      
-    }
-
 
     /** Walks forward. Keep walking till the new command from Jetson arrives...**/
     String walk()
     {
-      //set velocity before walk sequence...
-      this->setSpeed(70.0);
-      int wait = 350;
-      String nextCommand = CMD_EMPTY;
-      while(nextCommand == CMD_EMPTY) 
-      {
+       int wait = 500;
+       while(true) {
           delay(wait);
-          // to implement ...
+          leg_rL.moveTo(-6, 15);
+          leg_fL.moveTo(3, 12);
+          leg_rR.moveTo(3, 12);
+          leg_fR.moveTo(-6, 15);
           
-          nextCommand = getNextCommandFromJetson();
-      }
-      return nextCommand;
+          delay(wait);
+          leg_fL.moveTo(6, 15);
+          leg_rR.moveTo(6, 15);
+          
+          delay(wait);
+          leg_fL.moveTo(-6, 15);
+          leg_rL.moveTo(3, 12);
+          //...
+          leg_fR.moveTo(3, 12);
+          leg_rR.moveTo(-6, 15);
+
+          delay(wait);
+          leg_rL.moveTo(6, 15);
+          leg_fR.moveTo(6, 15);
+       }
+       return ""; 
+
+      
+//      //set velocity before walk sequence...
+//      this->setSpeed(70.0);
+//      int wait = 350;
+//      String nextCommand = CMD_EMPTY;
+//      while(nextCommand == CMD_EMPTY) 
+//      {
+//          delay(wait);
+//          // to implement ...
+//          
+//          nextCommand = getNextCommandFromJetson();
+//      }
+//      return nextCommand;
     }
     
 };
 
 /** Create robot object **/
-QuadrupedRobot quadrupedRobot = QuadrupedRobot();
+QuadrupedRobot robot = QuadrupedRobot();
 
 
 void setup() {
   Serial.begin(115200, SERIAL_8N1);                             // For Uno, Nano, Mini, and Mega, use UART port of DYNAMIXEL Shield to debug.
-//  while (!Serial) {
-//    ; // wait for serial port to connect.
-//  }
   shield.begin(57600);                                    // Set Port baudrate to 57600bps. This has to match with DYNAMIXEL baudrate.
   shield.setPortProtocolVersion(DXL_PROTOCOL_VERSION);    // Set Port Protocol Version. This has to match with DYNAMIXEL protocol version.
   delay(2000);
-  quadrupedRobot.configure();
+  robot.configure();
 }
 
 
@@ -425,14 +369,24 @@ String nextCmd = CMD_STOP;
 
 void loop() 
 {
-   // quadrupedRobot.stop();
+   robot.setSpeed(20.0);
 
-   // delay(2000);
-
-    quadrupedRobot.moveLegTo(100, 100);
-
-    delay(2000);
+   robot.homePosition();
+   delay(2000);
+   
+   robot.walk();
   
+//   robot.homePosition();
+
+//    while(true){
+//       delay(2000);
+//       robot.moveLegTo(4.0, 16.0);
+//       delay(2000);
+//       robot.moveLegTo(6.0, 20.0);
+//       delay(2000);
+//       robot.moveLegTo(1.0, 18.0);
+//    }
+    
 //  if(!nextCmd.equals(CMD_EMPTY)) 
 //  {
 //      Serial.println("Command received from master: ");
